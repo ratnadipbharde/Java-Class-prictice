d42e8b75559570511b78b575476eb59884620d78
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-07 17:20:26 +0530

    commit to change in main method

--- day1Practice/day1Practice/src/Main.java
+++ day1Practice/day1Practice/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("-----------program to display any message----------");
         DisplayMassage display=new DisplayMassage();
         display.Massage();
-        
+        System.out.println("-----------program to display the default value of all primitive data types of Java----------");
+        DisplaydefaultValue obj1 =new DisplaydefaultValue();
+        obj1.print();
 
     }
 }
