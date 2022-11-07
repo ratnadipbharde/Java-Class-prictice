ad1ded015550ab0c97d47dd1501ab1188457670f
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-07 17:48:46 +0530

    commit to push a program to check two strings are equal or not.

--- day1Practice/day1Practice/src/Main.java
+++ day1Practice/day1Practice/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("-----------program to display any message----------");
         DisplayMassage display=new DisplayMassage();
         display.Massage();
+
         System.out.println("-----------program to display the default value of all primitive data types of Java----------");
         DisplaydefaultValue obj1 =new DisplaydefaultValue();
         obj1.print();
+
+        System.out.println("-----------program to check two strings are equal or not----------");
+        CheckString check1=new CheckString("Mumbai","Mumbai");
+        check1.check();
 
     }
 }
